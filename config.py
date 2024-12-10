5ea150b8823b9871207a9e2455ab99fe7f269827
Danila Dolgov <cchilativadodd@gmail.com>
2024-12-10 17:42:14 +0400

    исправлены ошибки

--- config.py
+++ config.py
@@ -9,11 +9,33 @@ class Setting(BaseSettings):
     DB_NAME : str 
 
     @property
-    def Data_base_url_acyncpg(self):
-        #postgresql+asyncpg: //user: password@localhost/dbname
-        return f"postgresql+asyncpg: //{self.DB_USER}: {self.DB_PASS}@{self.DB_HOST}/{self.DB_NAME}"
+    def db_url(self):
+        #DSN
+        #postgresql+asyncpg://user:password@localhost:5342/dbname
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     model_config = SettingsConfigDict(env_file=".env")
 
 
 setting = Setting()
+
+
+# from dotenv import dotenv_values
+
+# config = dotenv_values(".env")
+
+# DB_HOST=config.get("DB_HOST")
+# DB_PORT=config.get("DB_PORT")
+# DB_USER=config.get("DB_USER")
+# DB_PASS=config.get("DB_PASS")
+# DB_NAME=config.get("DB_NAME")
+# DB_URL=f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+# class settings:
+#     DB_HOST=DB_HOST
+#     DB_PORT=DB_PORT
+#     DB_USER=DB_USER
+#     DB_PASS=DB_PASS
+#     DB_NAME=DB_NAME
+#     DB_URL=DB_URL
